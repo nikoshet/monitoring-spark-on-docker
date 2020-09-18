853373d8cbc5157453b7ca94a8013b84aa88cbd0
nikoshet <nnikitarrr@gmail.com>
2020-09-18 23:27:29 +0300

    updated node dashboard

--- Spark/app.py
+++ Spark/app.py
@@ -1,18 +1,23 @@
 import pyspark as ps
 import random
- 
+
 spark = ps.sql.SparkSession.builder \
         .appName("rdd test") \
         .getOrCreate()
- 
+
 random.seed(1)
- 
+#import time
+#time.sleep(1000)
+
 def sample(p):
     x, y = random.random(), random.random()
     return 1 if x*x + y*y < 1 else 0
  
-for x in range(0,10):
+for x in range(0,100):
     count = spark.sparkContext.parallelize(range(0, 10000000)).map(sample) \
-             .reduce(lambda a, b: a + b)
+        .map(lambda x: x*2) \
+        .map(lambda x: x - 10) \
+        .map(lambda x: x + 10) \
+        .reduce(lambda a, b: a + b)
  
     print("Pi is (very) roughly {}".format(4.0 * count / 10000000))
